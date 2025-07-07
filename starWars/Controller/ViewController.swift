a1f2b7f6979cd427dfaa306438f40461b4cbcfbe
Gabrielle <gabrielle97oliveira@gmail.com>
2025-07-07 11:09:33 +0100

    feat: criar projeto base no Xcode e configurar LaunchScreen

--- starWars/Controller/ViewController.swift
+++ starWars/Controller/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var characters: UIButton!
+    @IBOutlet weak var ships: UIButton!
+    @IBOutlet weak var planets: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
 
-
+    
 }
 
